1ba077f5a7d04f061067ecfb83684b63adc58f52
nokibsarkar <nokibsarkar@gmail.com>
2025-04-01 22:08:31 +0600

    added sample

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func initConfig() {
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
+}
+func initDB() {
+	username := viper.GetString("user")
+	password := viper.GetString("password")
+	database := "s55789__campwiz_test"
+	host := "tools.db.svc.wikimedia.cloud"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
+	log.Printf("Connecting to database with DSN: %s", dsn)
+	conn := mysql.Open(dsn)
+	if conn == nil {
+		log.Fatalf("Failed to create connection: %v", conn)
+	}
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+
+	}
+	res := db.Raw("SELECT 1").Scan(&struct{}{})
+	if res.Error != nil {
+		log.Fatalf("Failed to ping database: %v", res.Error)
+	}
+	co, err := db.DB()
+	if err != nil {
+		log.Fatalf("Failed to get DB instance: %v", err)
+	}
+	if err := co.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+	co.Close()
+
+}
+
+func main() {
+	initConfig()
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		initDB()
+		c.String(200, "Database connection successful")
+	})
+
+	r.Run()
+}
